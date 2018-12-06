81be017284358b5b559abad7eca3262c437e4b08
Ianhuuuuang <379248402@qq.com>
2018-12-06 13:14:35 +0800

    20181206

--- venv/file_move.py
+++ venv/file_move.py
@@ -5,5 +5,5 @@ for i in allList:
     aa, bb = i.split(".")
     if 'python' in aa.lower():
         oldName = u"D:\\notes\\python\\资料\\"+aa+"."+bb
-        newName = u"d:\\copy\\newname"+aa+"."+bb
+        newName = u"D:\\copy\\newname"+aa+"."+bb
         shutil.copyfile(oldname,newname)
