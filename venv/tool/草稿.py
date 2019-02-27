d2c9c666e7a84b43c48c2acc24d2a64fbdd4e609
Ianhuuuuang <379248402@qq.com>
2019-02-27 19:19:50 +0800

    20190227

--- venv/tool/草稿.py
+++ venv/tool/草稿.py
@@ -6,3 +6,10 @@ c = a.swapaxes(0,2)
 
 print(b)
 print(c[0])
+idx = np.where(a[:,:,0] == 15)
+print(np.where(a[:,:,1] <16))
+print(np.where(a[:,:,2] < 17))
+idx = zip(idx[0],idx[1])
+for x,y in idx:
+    a[x,y]=[100,100,100]
+print(a)
